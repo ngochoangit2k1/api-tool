45e4bc9a766980b587cf61b8cf3b57b7449f3a89
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2023-08-25 22:09:27 +0700

    update7

--- server/index.server.js
+++ server/index.server.js
@@ -14,6 +14,7 @@ export default async function data(req, res, next) {
     };
  
     const response = await axios.post(apiUrl, loginData);
+    console.log(response)
     const token = response.data;
     const tokenParts = token.split(".");
     const payload = JSON.parse(base64UrlDecode(tokenParts[1]));
